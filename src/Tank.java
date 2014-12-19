1c73a567e01b20883f9a2022dd8cbdf3cf4b7f77
MANU SINGHAL <singhalmanu9@gmail.com>
2014-12-18 19:53:13 -0500

    added several methods, still a lot of stuff to do

--- src/Tank.java
+++ src/Tank.java
@@ -13,13 +13,49 @@ import java.awt.Color;
  */
 public class Tank 
 {
-    public int position;
+    public static int muzzleSpeed = 200;
+    
+    public int myX;
+    public double angleFromHori;
     public int health;
     public int ammo;
     public Color myColor;
     
-    public Tank(Color col)
+    public Tank(Color c)
     {
-        myColor = col;
+        initTank(c);
+    }
+    
+    public void move(boolean direction)
+    {
+        if(direction)
+            myX ++;
+        else
+            myX --;
+    }
+    
+    
+    public void tiltAngle(boolean direction)
+    {
+        if(direction && angleFromHori > 0)
+            angleFromHori --;
+        else if(!direction && angleFromHori < Math.PI)
+            angleFromHori ++;
+    }
+    
+    public void shoot()
+    {
+        if(ammo > 0)
+        {
+            Bomb b = new Bomb(new Parabola(0, 0, new Vector(200, angleFromHori)));
+        }
+        else
+            System.out.println("NO AMMO");
+    }
+    
+    private void initTank(Color myColor)
+    {
+        angleFromHori = 0;
+        health = 100;
     }
 }
